139fc8cb3dc692e0dbb6b05f9a9f116b064b95c1
Gabriel Henrique <gabrielsilva.stk@gmail.com>
2024-07-02 20:44:02 -0300

    FEAT: ADICIONANDO JOGO DE PALAVRA ALEATORIA.

--- server/index.js
+++ server/index.js
@@ -7,14 +7,35 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://192.168.1.14:4200",
+    origin: "http://200.235.90.15:4200",
     methods: ["GET", "POST"]
   }
 });
 
 const PORT = process.env.PORT || 3000;
 let connectedUsers = 0;
-let users = {}; // Objeto para armazenar os nomes dos usuários
+let users = {}; 
+
+const palavras = [
+  'Elefante', 'Girafa', 'Leão', 'Tigre', 'Zebra', 'Rinoceronte', 'Hipopótamo', 'Crocodilo', 'Gorila', 'Macaco',
+  'Chimpanzé', 'Panda', 'Canguru', 'Coalho', 'Esquilo', 'Raposa', 'Lobo', 'Urso', 'Lontra', 'Castor',
+  'Foca', 'Baleia', 'Golfinho', 'Tubarão', 'Polvo', 'Lula', 'Caranguejo', 'Lagosta', 'Tartaruga', 'Cobra',
+  'Jacaré', 'Camaleão', 'Aranha', 'Escorpião', 'Abelha', 'Vespa', 'Formiga', 'Besouro', 'Borboleta', 'Libélula',
+  'Louva-a-deus', 'Gafanhoto', 'Grilo', 'Sapo', 'Rã', 'Salamandra', 'Axolote', 'Jabuti', 'Cágado', 'Pato',
+  'Ganso', 'Cisne', 'Pomba', 'Águia', 'Falcão', 'Coruja', 'Gaivota'
+];
+
+let palavraAtual = '';
+
+function escolherPalavraAleatoria() {
+  const indice = Math.floor(Math.random() * palavras.length);
+  palavraAtual = palavras[indice];
+
+  io.emit('message', { type: 'text',text: `Temos uma nova palavra`,  sender: 'Admin'})
+  io.emit('palavraAtual', palavraAtual);
+}
+
+escolherPalavraAleatoria();
 
 app.use(cors());
 app.use(express.static('client'));
@@ -23,33 +44,40 @@ io.on('connection', (socket) => {
   console.log('Cliente conectado:', socket.id);
   connectedUsers++;
 
-  // Evento para solicitar o nome do usuário
   socket.emit('request name');
 
-  // Quando o cliente envia seu nome
+ 
   socket.on('name', (name) => {
     console.log('Nome recebido:', name);
-    users[socket.id] = name; // Armazena o nome associado ao ID do socket
-    io.emit('userCount', { count: connectedUsers, users: Object.values(users) }); // Envia a contagem e os nomes para todos os clientes
+    users[socket.id] = name; 
+    io.emit('userCount', { count: connectedUsers, users: Object.values(users) });
     socket.broadcast.emit('message', { type: 'join', text: `${name} entrou no chat`, sender: name });
   });
 
-  // Quando o cliente se desconecta
   socket.on('disconnect', () => {
     console.log('Cliente desconectado:', socket.id);
     connectedUsers--;
     const name = users[socket.id];
     delete users[socket.id];
-    io.emit('userCount', { count: connectedUsers, users: Object.values(users) }); // Atualiza a contagem e os nomes para todos os clientes
+    io.emit('userCount', { count: connectedUsers, users: Object.values(users) });
     socket.broadcast.emit('message', { type: 'leave', text: `${name} desconectou`, sender: name });
   });
 
-  // Quando o cliente envia uma mensagem
   socket.on('send message', ({ text, sender }) => {
-    console.log('Mensagem recebida:', { text, sender });
+
     io.emit('message', { type: 'text', text, sender });
+    if (text.toLowerCase() === palavraAtual.toLowerCase()) {
+
+      io.emit('message', { type: 'text',text: `O usuário ${sender} acertou a palavra "${palavraAtual}"`,  sender: 'Admin'})
+      escolherPalavraAleatoria();
+    }
+
   });
+
+  socket.emit('palavraAtual', palavraAtual);
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Servidor está ouvindo na porta ${PORT}`);
